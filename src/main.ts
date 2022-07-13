0c2c5c54b03df2c0084024446a728df114dc926f
bertalanV <verebelyi.bertalan@zengo.eu>
2022-07-13 15:48:51 +0200

    feat: add pinia

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import Vue from 'vue';
+import { createPinia, PiniaVuePlugin } from 'pinia';
 import App from './App.vue';
 import router from './router';
 import vuetify from './plugins/vuetify';
@@ -7,8 +8,12 @@ import '@mdi/font/css/materialdesignicons.css';
 
 Vue.config.productionTip = false;
 
+Vue.use(PiniaVuePlugin);
+const pinia = createPinia();
+
 new Vue({
   router,
   vuetify,
+  pinia,
   render: (h) => h(App),
 }).$mount('#app');
